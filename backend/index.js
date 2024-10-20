bf5dfaf3671ae5ee3b4a496a2ccbdc7b75db8484
Vasanth26102004 <vasanth.p2610@gmil.com>
2024-10-20 16:55:11 +0530

    update 20/10

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ ENV_VARS;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://daily-do-app.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type','Authorization','user-id'],
   maxAge: 3600,
