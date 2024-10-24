6e64c000bc21241a9a95981d6f866b7bae931df6
Vasanth26102004 <vasanth.p2610@gmil.com>
2024-10-24 23:43:12 +0530

    commit

--- frontend/src/pages/TaskDetail/TaskDetail.jsx
+++ frontend/src/pages/TaskDetail/TaskDetail.jsx
@@ -11,7 +11,7 @@ const TaskDetail = () => {
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const response = await fetch("http://localhost:3000/task/alltask", {
+        const response = await fetch("https://daily-do-server.vercel.app/task/alltask", {
           method: "GET",
           headers: {
             Accept: "application/json",
