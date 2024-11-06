397b98e3746ee35f9390e4f9fe0f6d4673e6a448
Vasanth26102004 <vasanth.p2610@gmil.com>
2024-11-06 13:39:06 +0530

    image upload

--- frontend/src/pages/Upload/Upload.jsx
+++ frontend/src/pages/Upload/Upload.jsx
@@ -27,54 +27,60 @@ const Upload = () => {
 
   const handleSubmit = async () => {
     try {
-      // Prepare form data for image upload
+      let responseData;
+      let user = newUser;
+
       const formData = new FormData();
       formData.append("user", image);
-        alert(image);
-  
+
+      alert("image");
+
       // Upload image
-      const uploadResponse = await fetch(`https://daily-do-server.vercel.app/upload`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-        },
-        body: formData,
-      });
-  
-      alert(uploadResponse);
-      const uploadData = await uploadResponse.json();
-  
-      if (!uploadData.success) {
-        alert("Image upload failed");
-        return;
-      }
-  
-      // If image upload is successful, proceed with profile update
+      const uploadResponse = await fetch(
+        `https://daily-do-server.vercel.app/upload`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+          },
+          body: formData,
+        }
+      )
+        .then((resp) => resp.json())
+        .then((data) => {
+          responseData = data;
+        });
+
+      alert("uploadResponse");
+
       const userId = localStorage.getItem("user-id");
-      const updatedUser = { ...newUser, image: uploadData.image_url }; // Add image URL to user data
-  
-      const updateResponse = await fetch(`https://daily-do-server.vercel.app/auth/update/${userId}`, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedUser),
-      });
-  
-      const updateData = await updateResponse.json();
-  
-      if (updateData.success) {
-        alert("Profile Updated");
-      } else {
-        alert("Profile update failed");
+
+      if (responseData.success) {
+        user.image = responseData.image_url;
+        await fetch(
+          `https://daily-do-server.vercel.app/auth/update/${userId}`,
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+          }
+        )
+          .then((resp) => resp.json())
+          .then((data) => {
+            data.success
+              ? alert("Profile Updated")
+              : alert("Profile update failed");
+          });
       }
     } catch (error) {
       console.error("Error:", error.message);
       alert("An error occurred. Please try again.");
     }
   };
-  
+
   return (
     <div className="upload-container">
       <img class="background" src={shape} alt="" />
